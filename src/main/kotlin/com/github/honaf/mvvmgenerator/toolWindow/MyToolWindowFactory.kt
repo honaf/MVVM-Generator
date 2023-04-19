fbe934bdcfb28f8b8d32034388e41b39057a80a5
GitHub Action <action@github.com>
2023-04-19 08:36:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/honaf/mvvmgenerator/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/honaf/mvvmgenerator/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.honaf.mvvmgenerator.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.honaf.mvvmgenerator.MyBundle
+import com.github.honaf.mvvmgenerator.services.MyProjectService
 import javax.swing.JButton
 
 
